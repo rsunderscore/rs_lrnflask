37ee514069bbd9a89424a6851a3aea550715cc43
Robert Sunderland <rsunderscore@gmail.com>
2021-12-29 19:34:36 -0500

    initial code

---
+++ manage.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 16:32:42 2021
+
+@author: Rob
+"""
+#from main import app, db, User
+
+#@app.shell_context_processor
+#def make_shell_context():
+#    return dict(app=app, db=db, User=User)
